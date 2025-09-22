1ffc47d2cac5fbed326cfec5b0d92cc08d2b07d7
IsaAlegre <isabellaalegre84@gmail.com>
2025-09-22 03:27:55 -0300

    feat: add new relationships with reviews and libraries

--- backend/src/modules/books/bookEntity.ts
+++ backend/src/modules/books/bookEntity.ts
@@ -1,28 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany} from "typeorm";
+import { Library } from "../libraries/libraryEntity";
+import { Review } from "../reviews/reviewEntity";
 
 @Entity("books")
 export class Book {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column()
+  @Column({ type: "varchar" })
   title!: string;
 
-  @Column()
+  @Column({ type: "varchar" })
   author!: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, type: "varchar" })
   genre?: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, type: "int" })
   year?: number;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, type: "int" })
   pages?: number;
 
-  @Column({ nullable: true, type: "text" })
+  @Column({ nullable: true, type: "varchar" })
   description?: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, type: "varchar" })
   coverUrl?: string;
+
+  @OneToMany(() => Library, (library) => library.book)
+  libraryEntries!: Library[];
+
+  @OneToMany(() => Review, (review) => review.book)
+  reviews!: Review[];
 }
