adeba8ffb653fb4c905866faf753d755a88d59ec
IsaAlegre <isabellaalegre84@gmail.com>
2025-09-22 03:34:50 -0300

    feat: add new routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,15 @@
 // src/routes.ts
 import { Router } from "express";
-import bookRoutes from "./modules/books/bookRoutes.js";
+import bookRoutes from "./modules/books/bookRoutes";
+import userRoutes from "./modules/users/userRoutes"; // 1. Importar rutas de usuario
+import authRoutes from "./modules/auth/authRoutes"; // Importar rutas de autenticación
+
 
 const router = Router();
 
 // Rutas de cada módulo
+router.use("/auth", authRoutes);
 router.use("/books", bookRoutes);
+router.use("/users", userRoutes); 
 
 export default router;
