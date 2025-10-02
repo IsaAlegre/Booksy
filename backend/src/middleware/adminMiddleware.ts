52f924f9735162fe6eb8b14e6b5c0a843cc55f47
IsaAlegre <isabellaalegre84@gmail.com>
2025-10-02 18:58:53 -0300

    feat: add middleware for role admin

---
+++ backend/src/middleware/adminMiddleware.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from "express";
+import { UserRole } from "../modules/users/userEntity";
+
+export function adminMiddleware(req: Request, res: Response, next: NextFunction) {
+  const userRole = req.user?.role;
+
+  if (userRole !== UserRole.ADMIN) {
+    return res.status(403).json({ message: "Forbidden: Access denied. Admin role required." });
+  }
+
+  // Si el usuario es admin, permite que la petición continúe
+  next();
+}
