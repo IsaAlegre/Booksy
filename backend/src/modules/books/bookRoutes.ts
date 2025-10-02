69e677ffbb4df429127ab28b755892924156a075
IsaAlegre <isabellaalegre84@gmail.com>
2025-10-02 02:21:20 -0300

    refactor(books): organize book routes and mount nested review routes (mergeParams)

--- backend/src/modules/books/bookRoutes.ts
+++ backend/src/modules/books/bookRoutes.ts
@@ -1,13 +1,18 @@
 // src/modules/books/book.routes.ts
 import { Router } from "express";
 import { bookController } from "./bookController";
+import { authMiddleware } from "../../middleware/authMiddleware";
+import { adminMiddleware } from "../../middleware/adminMiddleware";
+import reviewRoutes from "../reviews/reviewRoutes";
 
 const router = Router();
 
 router.get("/", bookController.getAll.bind(bookController));
 router.get("/:id", bookController.getById.bind(bookController));
-router.post("/", bookController.create.bind(bookController));
-router.put("/:id", bookController.update.bind(bookController));
-router.delete("/:id", bookController.delete.bind(bookController));
+//rutas protegidas
+router.post("/", authMiddleware, adminMiddleware, bookController.create.bind(bookController));
+router.put("/:id",authMiddleware, adminMiddleware, bookController.update.bind(bookController));
+router.delete("/:id", authMiddleware, adminMiddleware, bookController.delete.bind(bookController));
+router.use("/:bookId/reviews", reviewRoutes);
 
 export default router;
