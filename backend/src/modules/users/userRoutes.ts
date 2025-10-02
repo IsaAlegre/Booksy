b09de347192168eb40d94574b4dcace2cb390f8c
IsaAlegre <isabellaalegre84@gmail.com>
2025-10-02 02:32:17 -0300

    refactor(users): separate public and protected routes; expose search and profile endpoints

--- backend/src/modules/users/userRoutes.ts
+++ backend/src/modules/users/userRoutes.ts
@@ -2,13 +2,18 @@ import { Router } from "express";
 import { userController } from "./userController";
 import { authMiddleware } from "../../middleware/authMiddleware";
 import libraryRoutes from "../libraries/libraryRoutes";
+import { adminMiddleware } from "../../middleware/adminMiddleware";
 
 const router = Router();
-router.use(authMiddleware);
+
+router.get("/search", userController.handleSearchUsers.bind(userController));
+
+// GET /api/users/123 -> Ver perfil público
+router.get("/:id", userController.handleGetProfile.bind(userController));
+
+router.get("/", authMiddleware, adminMiddleware, userController.getAll.bind(userController));
 
 router.use("/:userId/library", libraryRoutes);
-router.get("/", userController.getAll.bind(userController));
-router.post("/", userController.create.bind(userController));
 router.delete("/:id", authMiddleware, userController.delete.bind(userController));
 
 export default router;
