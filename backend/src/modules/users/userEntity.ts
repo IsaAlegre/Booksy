27f0a811027120016fbbe123185d6d3339d7e748
IsaAlegre <isabellaalegre84@gmail.com>
2025-10-02 02:31:38 -0300

    feat(users): add role column (default user) and role enum to user entity

--- backend/src/modules/users/userEntity.ts
+++ backend/src/modules/users/userEntity.ts
@@ -2,6 +2,11 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne } from "ty
 import { Library } from "../libraries/libraryEntity"; // Importar la entidad Library
 import { Review } from "../reviews/reviewEntity"; 
 
+export enum UserRole {
+  ADMIN = "admin",
+  USER = "user"
+}
+
 @Entity("users")
 export class User {
   @PrimaryGeneratedColumn()
@@ -15,6 +20,13 @@ export class User {
 
   @Column({ unique: true , type: "varchar" })
   password!: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role!: UserRole;
 
   @OneToMany(() => Library, (library) => library.user)
   libraryEntries!: Library[];
