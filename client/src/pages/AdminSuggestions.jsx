36291cf8d5b4476b8900901d54ef39938e58e8bb
IsaAlegre <isabellaalegre84@gmail.com>
2025-11-18 15:04:52 -0300

    feat: create AdminSuggestions page for admin dashboard

---
+++ client/src/pages/AdminSuggestions.jsx
@@ -0,0 +1,10 @@
+import AdminLayout from '../components/Admin/AdminLayaut';
+import SuggestionsList from '../components/Admin/SuggestionsList';
+
+export default function AdminSuggestions() {
+  return (
+    <AdminLayout>
+      <SuggestionsList />
+    </AdminLayout>
+  );
+}
